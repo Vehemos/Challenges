bce8e570fc234a42908c75caaab664204e9e990f
Sarthak Tripathi <vehemos@volker.viper.gtx>
2017-12-18 01:12:03 +0530

    Added a DailyProgrammer folder for my solutions to DP problems from that subreddit

---
+++ DailyProgrammer/guessgame.cpp
@@ -0,0 +1,22 @@
+# Challenge: https://www.reddit.com/r/dailyprogrammer/comments/pii6j/difficult_challenge_1/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int low = 1, high = 101;
+	int steps = 0, guess = (high+low)/2, ans;
+	while(ans != 5)
+		{
+			cout<<"My Guess is: "<<guess<<"\nIs it correct or too High/Low? (5 or 1/0)\n";
+			cin>>ans;
+			if(ans == 1)
+				guess = (guess+low)/2;
+			else
+				guess = (guess+high)/2;
+			steps++;
+		}
+	cout<<"I guessed your number in "<<steps<<" steps!";
+}
